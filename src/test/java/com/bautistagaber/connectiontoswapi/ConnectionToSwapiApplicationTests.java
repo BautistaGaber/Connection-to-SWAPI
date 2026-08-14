b9db5f2544ab700b53f135eb7e3c041af482fd69
BautistaGaber <bautistagaber@gmail.com>
2026-08-14 17:18:39 -0300

    init: initial commit

---
+++ src/test/java/com/bautistagaber/connectiontoswapi/ConnectionToSwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bautistagaber.connectiontoswapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionToSwapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
