0b6ec81079c1af8874c134fc12ebdab9f4bddcd9
BautistaGaber <bautistagaber@gmail.com>
2026-08-14 17:38:02 -0300

    feat: add people response mapping

---
+++ src/main/java/com/bautistagaber/connectiontoswapi/presentation/dto/response/PageResponse.java
@@ -0,0 +1,21 @@
+package com.bautistagaber.connectiontoswapi.presentation.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+
+    private int page;
+
+    private int size;
+
+    private int totalElements;
+
+    private int totalPages;
+}
---
+++ src/main/java/com/bautistagaber/connectiontoswapi/presentation/mapper/PeopleResponseMapper.java
@@ -0,0 +1,29 @@
+package com.bautistagaber.connectiontoswapi.presentation.mapper;
+
+import com.bautistagaber.connectiontoswapi.domain.model.People;
+import com.bautistagaber.connectiontoswapi.presentation.dto.response.PeopleResponse;
+
+public class PeopleResponseMapper {
+
+    public PeopleResponse toResponse(People people){
+        return PeopleResponse.builder()
+                .id(people.getId())
+                .name(people.getName())
+                .birthYear(people.getBirthYear())
+                .eyeColor(people.getEyeColor())
+                .films(people.getFilms())
+                .gender(people.getGender())
+                .hairColor(people.getHairColor())
+                .height(people.getHeight())
+                .homeworld(people.getHomeworld())
+                .mass(people.getMass())
+                .skinColor(people.getSkinColor())
+                .created(people.getCreated())
+                .edited(people.getEdited())
+                .species(people.getSpecies())
+                .starships(people.getStarships())
+                .url(people.getUrl())
+                .vehicles(people.getVehicles())
+                .build();
+    }
+}
