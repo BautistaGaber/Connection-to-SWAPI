dad5888fb4fe2fe9d6b4e8d63019fafa6065095b
BautistaGaber <bautistagaber@gmail.com>
2026-08-16 01:41:42 -0300

    feat: add main configuration

---
+++ src/main/java/com/bautistagaber/connectiontoswapi/ConnectionToSwapiApplication.java
@@ -0,0 +1,16 @@
+package com.bautistagaber.connectiontoswapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class
+        }
+)public class ConnectionToSwapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionToSwapiApplication.class, args);
+    }
+}
