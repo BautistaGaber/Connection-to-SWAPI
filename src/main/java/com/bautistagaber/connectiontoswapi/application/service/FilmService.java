9b2c6dea0003bffe9fb60d3c547fafbb12aa5d46
BautistaGaber <bautistagaber@gmail.com>
2026-08-16 16:28:07 -0300

    feat: add film service

---
+++ src/main/java/com/bautistagaber/connectiontoswapi/application/service/FilmService.java
@@ -0,0 +1,15 @@
+package com.bautistagaber.connectiontoswapi.application.service;
+
+import com.bautistagaber.connectiontoswapi.domain.model.Film;
+import com.bautistagaber.connectiontoswapi.domain.model.PageResult;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmService {
+    PageResult<Film> findFilms(int page, int size);
+
+    Optional<Film> findFilmById(Long id);
+
+    List<Film> findFilmByName(String name);
+}
---
+++ src/main/java/com/bautistagaber/connectiontoswapi/application/service/impl/FilmServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bautistagaber.connectiontoswapi.application.service.impl;
+
+import com.bautistagaber.connectiontoswapi.application.service.FilmService;
+import com.bautistagaber.connectiontoswapi.domain.model.Film;
+import com.bautistagaber.connectiontoswapi.domain.model.PageResult;
+import com.bautistagaber.connectiontoswapi.domain.port.out.SwapiPort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FilmServiceImpl implements FilmService {
+
+    private final SwapiPort swapiPort;
+
+    public FilmServiceImpl(SwapiPort swapiPort){
+        this.swapiPort = swapiPort;
+    }
+
+    @Override
+    public PageResult<Film> findFilms(int page, int size) {
+        return swapiPort.findFilms(page, size);
+    }
+
+    @Override
+    public Optional<Film> findFilmById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Film> findFilmByName(String name) {
+        return List.of();
+    }
+}
