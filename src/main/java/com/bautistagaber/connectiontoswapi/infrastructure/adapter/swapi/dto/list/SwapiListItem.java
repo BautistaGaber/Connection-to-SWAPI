a53d465f872be7f512081d44bbc9d6b49d29f18d
BautistaGaber <bautistagaber@gmail.com>
2026-08-17 03:30:36 -0300

    refactor: rename people list response

---
+++ src/main/java/com/bautistagaber/connectiontoswapi/infrastructure/adapter/swapi/dto/list/SwapiListItem.java
@@ -0,0 +1,8 @@
+package com.bautistagaber.connectiontoswapi.infrastructure.adapter.swapi.dto.list;
+
+
+//se necesita para listar todos los "people, starships y vehicles"
+public record SwapiListItem(String uid,
+                            String name,
+                            String url) {
+}
--- src/main/java/com/bautistagaber/connectiontoswapi/infrastructure/adapter/swapi/dto/list/SwapiPeopleListItem.java
+++
@@ -1,8 +0,0 @@
-package com.bautistagaber.connectiontoswapi.infrastructure.adapter.swapi.dto.list;
-
-
-//se necesita para listar todos lo "people"
-public record SwapiPeopleListItem( String uid,
-                                   String name,
-                                   String url) {
-}
